95cd12a6b390ee45d71ec90066eaecd7b7d3c47c
0xw01f <76235339+0xw01f@users.noreply.github.com>
2024-06-10 16:35:22 +0200

    Add Open Graph and Twitter Card meta tags for better social media sharing

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,18 @@ export default function RootLayout({ children }: { children: any }) {
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
         />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://beluga-delta.vercel.app/" />
+        <meta property="og:title" content="Beluga Tracker" />
+        <meta property="og:description" content="Trouvez les prochains attérisages de Beluga à votre aéroport !" />
+        <meta property="og:image" content="/og-image.png" />
+
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://beluga-delta.vercel.app/" />
+        <meta property="twitter:title" content="Beluga Tracker" />
+        <meta property="twitter:description" content="Trouvez les prochains attérisages de Beluga à votre aéroport !" />
+        <meta property="twitter:image" content="/og-image.png" />
       </head>
         <body>
           <MantineProvider theme={theme} defaultColorScheme="light">{children}</MantineProvider>
